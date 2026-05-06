63b5fc44b0607fbd9b164109045f4243309954f9
unknown <shaching.contact@gmail.com>
2026-05-07 03:40:08 +0600

    middleware setup

--- src/app.js
+++ src/app.js
@@ -1,4 +1,21 @@
 import e from "express";
 const app = e();
+import cookieParser from "cookie-parser";
+import cors from "cors";
+// middleware----
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(
+  e.json({
+    limit: "16kb",
+  })
+);
+app.use(e.urlencoded({ extended: true, limit: "16kb" }));
+app.use(e.static("public"));
+app.use(cookieParser());
 
 export { app };
