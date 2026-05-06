f95c4ef993fa3c431035878d121a21c60a22fb7f
unknown <shaching.contact@gmail.com>
2026-05-07 02:53:12 +0600

    db and server listen

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import dotennv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+const port = process.env.PORT || 3000;
 
 dotennv.config({
   path: "./",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`My server is running on ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("mongodb connection failed", err);
+  });
 
 /*
 (async () => {
