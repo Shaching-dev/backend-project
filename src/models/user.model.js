14f8603c0e79463798725b8168ef1353a6ae7400
unknown <shaching.contact@gmail.com>
2026-05-07 15:46:32 +0600

    middleware and file upload functionality implement

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -68,7 +68,7 @@ userSchema.methods.isPasswordCorrect = async function (password) {
 userSchema.methods.generateAccessToken = function () {
   return jwt.sign(
     {
-      _id: this_id,
+      _id: this._id,
       email: this.email,
       username: this.userName,
       fullname: this.fullName,
@@ -83,7 +83,7 @@ userSchema.methods.generateAccessToken = function () {
 userSchema.methods.generateRefreshToken = function () {
   return jwt.sign(
     {
-      _id: this_id,
+      _id: this._id,
     },
     process.env.REFRESH_TOKEN_SECRET,
 
