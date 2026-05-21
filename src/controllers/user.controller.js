516193c71954b5ba86a7a1b43b023d1cfb1ac71f
unknown <shaching.contact@gmail.com>
2026-05-21 12:13:03 +0600

    user registration login and logout set

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,70 +1,152 @@
-import { asyncHandler } from "../utils/asyncHandler.js";
-import { User } from "../models/user.model.js";
+import asyncHandler from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
-const registerUser = asyncHandler(async (req, res) => {
-  // console.log("request came");
+const generateAccessAndRefreshToken = async (userId) => {
+  const user = await User.findById(userId);
+  const accessToken = user.generateAccessToken();
+  const refreshToken = user.generateRefreshToken();
+  user.refreshToken = refreshToken;
 
+  await user.save({
+    validateBeforeSave: false,
+  });
+
+  return {
+    accessToken,
+    refreshToken,
+  };
+};
+
+const registerUser = asyncHandler(async (req, res) => {
   const { fullName, userName, email, password } = req.body;
-  // console.log(email, password);
+
   if (
-    [fullName, email, userName, password].some((field) => field?.trim() === "")
+    [fullName, userName, email, password].some((field) => field?.trim() === "")
   ) {
     throw new ApiError(400, "All fields are required");
   }
-  const existedUser = await User.findOne({
-    $or: [{ userName }, { email }],
+
+  if (!email.includes("@")) {
+    throw new ApiError(400, "valid email required");
+  }
+
+  if (password.length < 6) {
+    throw new ApiError(400, "password must be 6 character or more");
+  }
+
+  const exsitedUser = await User.findOne({
+    $or: [{ userName, email }],
   });
 
-  if (existedUser) {
-    throw new ApiError(409, "email and username already exist");
+  if (exsitedUser) {
+    throw new ApiError(400, "user already exist");
   }
-  const avatarLocal = req.files?.avatar[0]?.path;
-  let coverImageLocal;
+
+  const avatarLocalPath = req.files?.avatar[0].path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "avatar is required");
+  }
+
+  let coverImageLocalPath;
 
   if (
     req.files &&
     Array.isArray(req.files.coverImage) &&
-    req.files.coverImage.lenght > 0
+    req.files.coverImage.length > 0
   ) {
-    coverImageLocal = req.files.coverImage[0].path;
+    coverImageLocalPath = req.files?.coverImage[0].path;
   }
 
-  // console.log(req.files);
-
-  if (!avatarLocal) {
-    throw new ApiError(400, "Avatar files is required");
-  }
-
-  const avatar = await uploadOnCloudinary(avatarLocal);
-  const coverImage = await uploadOnCloudinary(coverImageLocal);
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
   if (!avatar) {
-    throw new ApiError(400, "Avatar is required");
+    throw new ApiError(400, "avatar is required");
   }
 
-  const user = await User.create({
+  const user = {
     fullName,
-    avatar: avatar?.url,
-    coverImage: coverImage?.url || "",
-    userName: userName.toLowerCase(),
     email,
     password,
-  });
+    userName: userName.toLowerCase(),
+    avatar: avatar?.url,
+    coverImage: coverImage?.url || "",
+  };
 
   const createdUser = await User.findById(user._id).select(
     "-password -refreshToken"
   );
 
   if (!createdUser) {
-    throw new ApiError(500, "Something went wrong in registration user");
+    throw new ApiError(
+      500,
+      "something went wrong while creating user. Please try again"
+    );
   }
 
   return res
-    .status(201)
-    .json(new ApiResponse(200, createdUser, "User registered successfully"));
+    .status(200)
+    .json(new ApiResponse(201, createdUser, "user created successfully"));
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+  const { userName, email, password } = req.body;
+
+  if (!userName || !email) {
+    throw new ApiError(400, "username or email is required");
+  }
+
+  const user = User.findOne({
+    $or: [{ userName }, { email }],
+  });
+
+  if (!user) {
+    throw new ApiError(404, "user not found");
+  }
+
+  const validatePassword = user.isPasswordCorrect(password);
+
+  if (!validatePassword) {
+    throw new ApiError(400, "invalid user credentials");
+  }
+
+  // generate access and refresh token here ---
+
+  const { accessToken, refreshToken } =
+    await generateAccessAndRefreshToken(password);
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .satus(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken,
+          refreshToken,
+        },
+        "user logged in successfully"
+      )
+    );
+});
+
+const logOutUser = asyncHandler(async (req, res) => {
+  // first i need to find the user id who want to log out then update refresh token undefined
+});
+
+export { registerUser, loginUser };
---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+
+const verifyJWT = asyncHandler(async (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.headers("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+      throw new ApiError(401, "unauthorized request");
+    }
+
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findById(decoded._id).select(
+      "-password, -refreshToken"
+    );
+
+    if (!user) {
+      throw new ApiError(401, "invalid user access");
+    }
+    req.user = user;
+
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "invalid user access");
+  }
+});
